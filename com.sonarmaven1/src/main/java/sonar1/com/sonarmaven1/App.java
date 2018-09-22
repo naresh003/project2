d32a6952bb73c67656100c6563871685185c2ddf
naresh <naresh91.linux@gmail.com>
2018-09-22 12:58:46 -0400

    com.sonarmaven1/

---
+++ com.sonarmaven1/src/main/java/sonar1/com/sonarmaven1/App.java
@@ -0,0 +1,13 @@
+package sonar1.com.sonarmaven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
